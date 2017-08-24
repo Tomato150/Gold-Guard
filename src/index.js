2210b97ccf64d6e48e45d8fca2610801bcdd2e1b
BuildTools <unconfigured@null.spigotmc.org>
2017-08-24 19:21:01 +1000

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class App extends React.Component {
+    render() {return(
+        <p>Gold Guard</p>
+    )}
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
