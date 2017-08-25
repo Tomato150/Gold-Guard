733779f6e40f56f2483daa997d38d51c05b4cb71
Tomato150 <tomcat.frantz@gmail.com>
2017-08-25 18:18:53 +1000

    Started work on react-konva canvas alternative.
    Works better than manual canvas, will be preferred style of canvas

--- src/index.js
+++ src/index.js
@@ -1,65 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Layer, Stage, Image} from 'react-konva';
 
 import trialImageSRC from './trial.jpg'
 
-class App extends React.Component {
-    /** @namespace this.refs.gameCanvas */
-    constructor (props) {
+class GameMapImage extends React.Component {
+    constructor(props) {
         super(props);
-        this.state = {
-            gameCanvasMouseDown: null,
-            gameCanvasMoved: false,
-            initialX: 0,
-            initialY: 0
-        };
-        this.canvasMouseUp = this.canvasMouseUp.bind(this);
-        this.canvasMouseDown = this.canvasMouseDown.bind(this);
-        this.canvasMouseMove = this.canvasMouseMove.bind(this);
+        this.state = {image: null}
     }
 
-    render () { return (
-        <canvas
-            ref="gameCanvas"
-            onMouseUp={this.canvasMouseUp}
-            onMouseDown={this.canvasMouseDown}
-            onMouseMove={this.state.gameCanvasMouseDown ? this.canvasMouseMove : null}
-            width="3000px"
-            height="4200px" >
-            You need to un-fuck yourself to run this
-        </canvas>
+    render () {return(
+        <Image
+            draggable="true"
+            image={this.state.image}
+            onMouseDown={function (e) {console.log(e)}}
+        />
     )}
 
     componentDidMount () {
-        const ctx = this.refs.gameCanvas.getContext('2d');
-
         const image = new window.Image();
         image.src = trialImageSRC;
         image.onload = () => {
-            ctx.drawImage(image, 0, 0)
-        }
+            this.setState({image: image})
+        };
+    }
+}
+
+class App extends React.Component {
+    constructor (props) {
+        super(props);
     }
 
-    canvasMouseUp (e) {
-        e.preventDefault();
-        if (!this.state.gameCanvasMoved) {
-            // preform click action here
-        }
-        this.setState({gameCanvasMouseDown: false, gameCanvasMoved: false});
-        console.log("Mouse Up");
-    }
-
-    canvasMouseDown (e) {
-        e.preventDefault();
-        this.setState({gameCanvasMouseDown: true});
-        console.log("Mouse Down");
-    }
-
-    canvasMouseMove (e) {
-        this.setState({gameCanvasMoved: true});
-        console.log("Mouse Moving");
-    }
+    render () { return (
+        <Stage width={100} height={100}>
+            <Layer>
+                <GameMapImage/>
+            </Layer>
+        </Stage>
+    )}
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
